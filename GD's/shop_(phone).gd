1a38ee2d934d0995fe8fc8a35c729c6519ef2d0d
kirchemz <kirchemz@gmail.com>
2026-02-08 11:41:34 -0500

    Phone Size Test 12

--- GD's/shop_(phone).gd
+++ GD's/shop_(phone).gd
@@ -69,7 +69,7 @@ func _process(_delta: float) -> void:
 
 
 func _on_next_level_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+	get_tree().change_scene_to_file("res://Scenes/main_(phone).tscn")
 	Globals.goal += 20
 
 
